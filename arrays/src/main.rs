10022c3d07ee53992fdd4ab1489e9688f4377f84
Shohei Jinno <equivalentexchange4645@gmail.com>
2020-11-02 14:40:04 -0800

    Finish validate sequence.

--- arrays/src/main.rs
+++ arrays/src/main.rs
@@ -1,16 +1,20 @@
 fn main() {
-    let mut arr = vec![3, 1, 2, -4, 5];
-    let target = 6;
+    // let mut arr = vec![3, 1, 2, -4, 5];
+    // let target = 6;
 
-    // arr.sort();
-    // println!("{:?}", arr);
-    println!("{:?}", two_num_sum(&mut arr, target));
+    // // arr.sort();
+    // // println!("{:?}", arr);
+    // println!("{:?}", two_num_sum(&mut arr, target));
+    let mut arr1 = vec![2, 3, 4, 5, 6];
+    let mut arr2 = vec![3, 5];
+
+    println!("{}", validate_sequence(&mut arr1, &mut arr2));
 }
 
-// two number sum
+// TWO NUMBER SUM
 /*
-Given an array of integers and a target value, return an array of two integers that sum up to the target value.
-Return an empty array otherwise.
+    Given an array of integers and a target value, return an array of two integers that sum up to the target value.
+    Return an empty array otherwise.
 */
 fn two_num_sum(arr: &mut Vec<i32>, target: i32) -> Vec<i32> {
     arr.sort();
@@ -34,6 +38,29 @@ fn two_num_sum(arr: &mut Vec<i32>, target: i32) -> Vec<i32> {
     vec![]
 }
 
+// VALIDATE SEQUENCE
+/*
+    Given two non-empty arrays, validate that the second array is a subsequence of the first.
+*/
+fn validate_sequence(arr1: &mut Vec<i32>, arr2: &mut Vec<i32>) -> bool {
+    arr1.sort();
+    arr2.sort();
+
+    let mut first = 0;
+    let mut second = 0;
+
+    while first < arr1.len() && second < arr2.len() {
+        if arr2[second] == arr1[first] {
+            first += 1;
+            second += 1;
+        } else {
+            first += 1;
+        }
+    }
+
+    second == arr2.len()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -46,5 +73,18 @@ mod tests {
         assert_eq!(two_num_sum(&mut vec![3, 3, 3, 3, 2, 4], 5), vec![2, 3]);
         assert_eq!(two_num_sum(&mut vec![-5, 4, -1, 3], -6), vec![-5, -1]);
         assert_eq!(two_num_sum(&mut vec![100, 0], 99), vec![]);
+    }
+
+    #[test]
+    fn test_validate_sequence() {
+        assert_eq!(validate_sequence(&mut vec![1, 2, 3], &mut vec![2]), true);
+        assert_eq!(
+            validate_sequence(&mut vec![5, 4, 3, 6, 1], &mut vec![3, 4]),
+            true
+        );
+        assert_eq!(
+            validate_sequence(&mut vec![-100, 0, 4, 5], &mut vec![]),
+            true
+        );
     }
 }
